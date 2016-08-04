a113a805deba10d04075fb9d523554dfbfdcfabd
Kyu Taek Hwang <kyutaekhwang@gmail.com>
2016-08-04 02:02:39 -0400

    updated html, css, and js files

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+ 	$("#yes").on('click', function() {
+ 		$(".begin, .question1").show();
+ 		$(".question2, .end, .statement1, .statement2, .statement3").hide();
+ 	$('html,body').animate({
+        scrollTop: $(".question1").offset().top},
+        'slow');
+ 	});
+
+ 	$("#no").on('click', function() {
+ 		$(".begin, .statement1").show();
+ 		$(".statement2, .statement3, .question1, .question2, .end").hide();
+ 	$('html,body').animate({
+        scrollTop: $(".statement1").offset().top},
+        'slow');
+ 	});
+
+ 	$("#yes1").on('click', function() {	
+ 		$(".begin, .question1, .question2").show();
+ 		$(".end, .statement1, .statement2, .statement3").hide();
+  	$('html,body').animate({
+        scrollTop: $(".question2").offset().top},
+        'slow');
+ 	});
+
+ 	$("#no1").on('click', function() {	
+ 		$(".begin, .question1, .statement2").show();
+ 		$(".question2, .end, .statement1, .statement3").hide();
+ 	$('html,body').animate({
+        scrollTop: $(".statement2").offset().top},
+        'slow');
+ 	});
+
+ 	$("#yes2").on('click', function() {
+ 		$(".begin, .question1, .question2, .end").show();
+ 		$(".statement1, .statement2, .statement3").hide();
+	$('html,body').animate({
+        scrollTop: $(".end").offset().top},
+        'slow');
+ 	});
+
+ 	$("#no2").on('click', function() {
+ 		$(".begin, .question1, .question2, .statement3").show();
+ 		$(".end, .statement1, .statement2").hide();
+ 	$('html,body').animate({
+        scrollTop: $(".statement3").offset().top},
+        'slow'); 		
+ 	});
+});
