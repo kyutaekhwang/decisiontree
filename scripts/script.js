e63595db4cc826a29660b7125c36ce7bdd6feac5
Kyu Taek Hwang <kyutaekhwang@gmail.com>
2016-08-04 19:28:19 -0400

    updates after demonstration

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
- 	$(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.top').fadeIn();
-        } else {
-            $('.top').fadeOut();
-        }
-    });
-
     $('.top').click(function () {
         $("html, body").animate({
             scrollTop: 0
